dbe5a25c2c9c70bd845e9c5f0519e8edc1f6ec34
Matt Mueller <mattmuelle@gmail.com>
2012-11-09 10:57:17 -0800

    merged matthewmueller/array support to collection

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  * Module dependencies.
  */
 
-var Enumerable = require('enumerable');
+var Enumerable = require('enumerable'),
+    Emitter = require('emitter');
 
 /**
  * Expose `Collection`.
@@ -29,6 +30,12 @@ function Collection(models) {
 Enumerable(Collection.prototype);
 
 /**
+ * Mixin emitter
+ */
+
+Emitter(Collection.prototype);
+
+/**
  * Iterator implementation.
  */
 
@@ -52,13 +59,101 @@ Collection.prototype.length = function(){
 };
 
 /**
- * Add `model` to the collection and return the index.
+ * Removes the last element from an array and returns that element
  *
- * @param {Object} model
- * @return {Number}
- * @api public
+ * @return {Mixed} removed element
  */
 
-Collection.prototype.push = function(model){
-  return this.models.push(model);
+Collection.prototype.pop = function() {
+  var ret = this.models.pop.apply(this, arguments);
+  this.emit('pop', ret);
+  this.emit('remove', ret);
+  return ret;
+};
+
+/**
+ * Push a value onto the end of the array,
+ * returning the length of the array
+ *
+ * @param {Mixed, ...} elements
+ * @return {Number}
+ */
+
+Collection.prototype.push = function() {
+  var ret = this.models.push.apply(this, arguments),
+      args = [].slice.call(arguments);
+  this.emit('push', ret);
+  for(var i = 0, len = args.length; i < len; i++) this.emit('add', args[i]);
+  return ret;
+};
+
+/**
+ * Reverses an array in place.
+ *
+ * @return {Array}
+ */
+
+Collection.prototype.reverse = function() {
+  var ret = this.models.reverse.apply(this, arguments);
+  this.emit('reverse', ret);
+  return ret;
+};
+
+/**
+ * Removes the first element from an array and returns that element.
+ *
+ * @return {Mixed}
+ */
+
+Collection.prototype.shift = function() {
+  var ret = this.models.shift.apply(this, arguments);
+  this.emit('shift', ret);
+  this.emit('remove', ret);
+  return ret;
+};
+
+/**
+ * Sorts the elements of an array.
+ *
+ * @return {Array}
+ */
+
+Collection.prototype.sort = function() {
+  var ret = this.models.sort.apply(this, arguments);
+  this.emit('sort', ret);
+  return ret;
+};
+
+/**
+ * Adds and/or removes elements from an array.
+ *
+ * @param {Number} index
+ * @param {Number} howMany
+ * @param {Mixed, ...} elements
+ * @return {Array} removed elements
+ */
+
+Collection.prototype.splice = function() {
+  var ret = this.models.splice.apply(this, arguments),
+      added = [].slice.call(arguments, 2);
+  this.emit('splice', ret);
+  for(var i = 0, len = ret.length; i < len; i++) this.emit('remove', ret[i]);
+  for(    i = 0, len = added.length; i < len; i++) this.emit('add', added[i]);
+  return ret;
+};
+
+/**
+ * Adds one or more elements to the front of an array
+ * and returns the new length of the array.
+ *
+ * @param {Mixed, ...} elements
+ * @return {Number} length
+ */
+
+Collection.prototype.unshift = function() {
+  var ret = this.models.unshift.apply(this, arguments),
+      args = [].slice.call(arguments);
+  this.emit('unshift', ret);
+  for(var i = 0, len = args.length; i < len; i++) this.emit('add', args[i]);
+  return ret;
 };
